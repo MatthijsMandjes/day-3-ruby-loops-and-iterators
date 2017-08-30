3dd04f7bc689e8100115e1d1c9d6d200f1ab8160
Matthijs <matthijs_mandjes@hotmail.com>
2017-08-30 15:24:03 +0200

    Finished Spanish omelette

---
+++ yoga_store.rb
@@ -0,0 +1,10 @@
+shopping_cart = []
+
+@products = [
+  { id: 1231, name: "Coca cola", price: 10 },
+  { id: 1232, name: "Milk", price: 5 },
+  { id: 1233, name: "Banana", price: 2 },
+  { id: 1234, name: "Cabbage", price: 4 },
+  { id: 1235, name: "Cheese", price: 8 },
+  { id: 1236, name: "Ice cream", price: 100 }
+]
